00a0cf9c6841bab1b48f63d868f245bbe26320b0
jakebalula <balula@hartford.edu>
2025-03-18 12:35:53 -0400

    created primeGenerator function

--- main.go
+++ main.go
@@ -2,22 +2,33 @@ package main
 
 import (
   "math"
+  "sync"
 )
 
 func isPrime(n int) bool {
   if n <= 2 {
     return false
   }
-  for i := 2; i <= int(math.Sqrt(float64(n))); i++ {
-    if n%2 == 0 {
+  for i := 2; i <= int(math.Sqrt(float64(n))); i++ { //Algorithm to check for prime numbers
+    if n%i == 0 {
       return false
     }
   }
   return true
 }
-func primeGenerator {
+func primeGenerator(start, end int, ch chan<- int, wg *sync.WaitGroup) {
+  defer wg.Done() //Makes sure the goroutine signals completion
+
+  for i := start; i <= end; i++ {
+    if isPrime(i) {
+      ch <- i //Checks if i is prime and sends it to the channel
+    }
+  }
+}
+
+func conPrimes(limit int, numWorkers int) []int {
   
 }
-func main(){
+func main() {
 
 }
