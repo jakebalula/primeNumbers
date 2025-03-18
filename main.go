3f03cea58e9e909ae55f63d32300c95382c4bda8
jakebalula <balula@hartford.edu>
2025-03-18 12:54:52 -0400

    Function concurrent Primes

--- main.go
+++ main.go
@@ -26,9 +26,29 @@ func primeGenerator(start, end int, ch chan<- int, wg *sync.WaitGroup) {
   }
 }
 
-func conPrimes(limit int, numWorkers int) []int {
-  
+func conPrimes(limit int, numWorkers int) []int { //Finds primes using multiple goroutines
+  ch := make(chan int, limit) //Creates channel for primes
+  var wg sync.WaitGroup
+  step := limit / numWorkers
+  primes := []int{}
+
+  for i := 0; i < numWorkers; i++ {
+    start := i*step + 1
+    end := (i + 1) * step
+    wg.Add(1)
+    go primeGenerator(start, end, ch, &wg)
+  }
+  go func() {
+    wg.Wait()
+    close(ch)
+  }()
+
+  for prime := range ch {
+    primes = append(primes, prime) //Collects the primes
+  }
+  return primes
 }
+
 func main() {
 
 }
